ef5efbddaf4b5b133fdd254709bbc8095f63d8c6
Alexandre Duvois <alex.duvois@pm.me>
2020-12-05 23:22:53 +0100

    Use cors and body-parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,14 @@
 import * as Koa from 'koa';
 import MainController from './Controller/MainController';
 import {KoaLogger} from './Middleware/KoaLogger';
+import * as bodyParser from 'koa-bodyparser';
+import * as cors from '@koa/cors';
 
 const app = new Koa();
 
 app.use(KoaLogger());
+app.use(cors());
+app.use(bodyParser());
 app.use(MainController.routes()).use(MainController.allowedMethods());
 
 export {app};
