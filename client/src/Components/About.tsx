c368f1f9c10f36385be91f38e4040d8667470480
Alexandre Duvois <alex.duvois@pm.me>
2020-12-24 23:36:26 +0100

    Change side of about picture

--- client/src/Components/About.tsx
+++ client/src/Components/About.tsx
@@ -11,10 +11,11 @@ function About() {
                     <h2>A passion for Cybersecurity</h2>
                 </div>
                 <div className="infos">
+                    <img src={setup} alt=""/>
                     <div className="text">
                         <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolore dolorum earum enim, impedit ipsam ipsum laudantium, minima numquam pariatur perferendis placeat porro quae reprehenderit similique! Architecto in perspiciatis veritatis.</span><span>Alias cupiditate et excepturi maxime nihil odio optio quod sapiente? Ab, assumenda aut consequatur cupiditate est eum in iure magnam magni minima minus, nostrum placeat possimus quia repellendus repudiandae ut.</span><span>Excepturi in incidunt iusto necessitatibus omnis quam reprehenderit sed totam! Ad aliquid aut deserunt dignissimos dolor doloremque exercitationem facere harum ipsam iure minima modi nihil nisi porro quasi recusandae, sed.</span><span>Consectetur cumque dicta facere officiis. Architecto eveniet hic id illo impedit iste odit possimus sapiente tempore? Amet blanditiis dolore explicabo laboriosam nulla officia placeat quibusdam quos vero voluptatem. Iste, rerum.</span><span>Cumque dignissimos eaque enim est excepturi exercitationem explicabo facere facilis libero nisi qui quia quisquam repellat repellendus, repudiandae tenetur vitae? Aperiam dolorum eos et excepturi ipsam maxime non quam. Aliquid.</span></p>
                     </div>
-                    <img src={setup} alt=""/>
+
                 </div>
                 <div className="location">
                     <h3><span className="title">Living in</span><br/>Strasbourg</h3>
